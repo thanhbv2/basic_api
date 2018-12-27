9adb89dc36b0451314dfc9b2428cd7036b55eb8c
thanh_buivan_vn <thanh.buivan@acsendcorp.com>
2018-12-27 21:36:55 +0700

    fix

--- myapp/app.js
+++ myapp/app.js
@@ -1,26 +1,35 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// var createError = require('http-errors');
+// var express = require('express');
+// var path = require('path');
+// var cookieParser = require('cookie-parser');
+// var logger = require('morgan');
+// const cors = require('cors')
+// var indexRouter = require('./controllers');
+// const db = require('./models');
 
-var indexRouter = require('./routes/index');
-const db = require('./models');
+
+// var app = express();
+
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+// app.use(cors())
+// app.use('/', indexRouter);
 
 
-var app = express();
+// module.exports = app;
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+const express = require('express');
+const cors = require('cors');
 
-app.use('/', indexRouter);
+const app = express();
+
+const router = require('./controllers');
+app.use(cors());
+app.use('/', router);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -38,4 +47,8 @@ app.use(function (err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+
+
+
+
+app.listen(3000, () => console.log('===============>server run port 3000', ))
