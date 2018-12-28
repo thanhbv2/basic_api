cee6fe7d0c8dc5ea4f039cef9f42a0c40b9ef8ea
thanh_buivan_vn <thanh.buivan@acsendcorp.com>
2018-12-28 15:49:51 +0700

    update

--- myapp/app.js
+++ myapp/app.js
@@ -1,35 +1,22 @@
-// var createError = require('http-errors');
-// var express = require('express');
-// var path = require('path');
-// var cookieParser = require('cookie-parser');
-// var logger = require('morgan');
-// const cors = require('cors')
-// var indexRouter = require('./controllers');
-// const db = require('./models');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const cors = require('cors')
+var indexRouter = require('./controllers');
+const db = require('./models');
 
 
-// var app = express();
+var app = express();
 
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-// app.use(cors())
-// app.use('/', indexRouter);
-
-
-// module.exports = app;
-
-
-const express = require('express');
-const cors = require('cors');
-
-const app = express();
-
-const router = require('./controllers');
-app.use(cors());
-app.use('/', router);
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
+app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -48,7 +35,36 @@ app.use(function (err, req, res, next) {
 });
 
 
+module.exports = app;
+
+
+// const express = require('express');
+// const cors = require('cors');
+
+// const app = express();
+
+// const router = require('./controllers');
+// app.use(cors());
+// app.use('/', router);
+
+// // catch 404 and forward to error handler
+// app.use(function (req, res, next) {
+//   next(createError(404));
+// });
+
+// // error handler
+// app.use(function (err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 
 
-app.listen(3000, () => console.log('===============>server run port 3000', ))
+
+
+// app.listen(3000, () => console.log('===============>server run port 3000', ))
